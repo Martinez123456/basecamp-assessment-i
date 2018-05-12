001f0c1df5795858cde19fbb9393a691ef95eacd
unknown <esmeraldamartinez889@gmail.com>
2018-05-12 16:29:20 -0700

    JavaScript Assessment Completed

--- basecamp-assessment-i/assessment.js
+++ basecamp-assessment-i/assessment.js
@@ -1,5 +1,5 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
-var myName = "Esmeralda";
+var myName = 'Esmeralda';
 // CODE HERE
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
@@ -11,7 +11,7 @@ var lovesCode = true;
 // CODE HERE
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
-var me = "My name is " + myName + " and my favorite number is " + faveNum;
+var me = 'My name is ' + myName + ' and my favorite number is ' + faveNum;
 // CODE HERE
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. 
@@ -26,7 +26,7 @@ else {
 // CODE HERE
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
-var notHakeem = "Esmeralda" && notHakeem != "Hakeem Olajuwon";
+var notHakeem = 'Esmeralda' && notHakeem != 'Hakeem Olajuwon';
 // CODE HERE
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
@@ -48,14 +48,14 @@ else {
 var colorResponse;
 
 var responseCreator = function(param){
-  if (param == "green"){
-    colorResponse ="Green is okay";
+  if (param == 'green'){
+    colorResponse ='Green is okay';
   }
-  else if (param == "red"){
-    colorResponse = "I heart red";
+  else if (param == 'red'){
+    colorResponse = 'I heart red';
   }
   else {
-    colorResponse = "What is your favorite color?";
+    colorResponse = 'What is your favorite color?';
   };
   return colorResponse 
 }
@@ -68,7 +68,7 @@ function sum(number){
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 function greeter(name, number){
-return "Hi! I am " + name + " and I am " + number + " years old";
+return 'Hi! I am ' + name + ' and I am ' + number + ' years old';
 };
 greeter(Esmeralda, 18);
 // CODE HERE
